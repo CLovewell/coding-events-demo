1395de366c5ad201ea057724094df6bf6c76a735
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-10-29 22:07:38 -0500

    16.2.1 Enum Types in Models

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -23,16 +23,19 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
+    private EventType type;
+
     public Event() {
         this.id = nextId;
         nextId++;
     }
 
-    public Event(String name, String description, String contactEmail) {
+    public Event(String name, String description, String contactEmail, EventType type) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.type = type;
     }
 
     public String getName() {
@@ -57,6 +60,14 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     public int getId() {
