b4f2212fb9e6a7156439632ca4097f0c465f1b19
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-11-16 22:20:42 -0600

    Collected functionality common to all entities in abstract class AbstractEntity

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,18 +1,11 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventCategory extends AbstractEntity {
 
     @NotBlank(message = "Name cannot be left blank.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
@@ -26,10 +19,6 @@ public class EventCategory {
 
     public EventCategory() {}
 
-    public int getId() {
-        return id;
-    }
-
     public String getName() {
         return name;
     }
@@ -41,18 +30,5 @@ public class EventCategory {
     @Override
     public String toString() {
         return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EventCategory that = (EventCategory) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
