b73032bbd2291c1cd3738323ecf686364b373604
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-11-19 14:58:47 -0600

    Established many-to-many relationship between entity Event and entity Tag

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -4,8 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Chris Bay
@@ -16,6 +16,9 @@ public class Tag extends AbstractEntity {
     @Size(min = 1, max = 25)
     @NotBlank
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final Set<Event> events = new HashSet<>();
 
     public Tag(String name) {
         this.name = name;
@@ -35,4 +38,7 @@ public class Tag extends AbstractEntity {
         this.name = name;
     }
 
+    public Set<Event> getEvents() {
+        return events;
+    }
 }
