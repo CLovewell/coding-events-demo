832a9a3c4badd92db2f8d7a009cdc9ebc4c5af3a
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-11-19 16:32:23 -0600

    Video lesson 18.5.5 complete. Add tag to event UI and database functionality.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,8 +2,11 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.data.EventRepository;
+import org.launchcode.codingevents.data.TagRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventCategory;
+import org.launchcode.codingevents.models.Tag;
+import org.launchcode.codingevents.models.dto.EventTagDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +28,9 @@ public class EventController {
 
     @Autowired
     private EventCategoryRepository eventCategoryRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
 
     @GetMapping
     public String displayEvents(@RequestParam(required = false) Integer categoryId, Model model) {
@@ -96,9 +102,44 @@ public class EventController {
             Event event = result.get();
             model.addAttribute("title", event.getName() + " Details");
             model.addAttribute("event", event);
+            model.addAttribute("tags", event.getTags());
         }
 
         return "events/detail";
     }
 
+    @GetMapping("add-tag")
+    public String displayAddTagForm(@RequestParam Integer eventId, Model model) {
+
+        Optional<Event> result = eventRepository.findById(eventId);
+        if (result.isPresent()) {
+            Event event = result.get();
+            model.addAttribute("title", "Add tag to: " + event.getName());
+            model.addAttribute("tags", tagRepository.findAll());
+            EventTagDTO eventTag = new EventTagDTO();
+            eventTag.setEvent(event);
+            model.addAttribute("eventTag", eventTag);
+            return "events/add-tag";
+        }
+        else {
+            model.addAttribute("title", "Invalid Event ID: " +
+                    eventId);
+            return "redirect:";
+        }
+    }
+
+    @PostMapping("add-tag")
+    public String processAddTagForm(@ModelAttribute @Valid EventTagDTO eventTag,
+                                    Errors errors, Model model) {
+        if (!errors.hasErrors()) {
+            Tag tag = eventTag.getTag();
+            Event event = eventTag.getEvent();
+            event.addTag(tag);
+            eventRepository.save(event);
+            return "redirect:detail?eventId=" + event.getId();
+        }
+        else {
+            return "redirect:add-tag";
+        }
+    }
 }
