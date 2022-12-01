4a6ef6c257825fad0045a1f7e0e9b61879d675b3
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-12-01 12:38:26 -0600

    User model and UserRepository created.

--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -2,12 +2,10 @@ package org.launchcode.codingevents.data;
 
 import org.launchcode.codingevents.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-/**
- * Created by Chris Bay
- */
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
-
 }
